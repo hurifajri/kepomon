ff6ad5662399841cca234e479683ecb5273d276e
Muhamad Fajri <hurifajri@gmail.com>
2021-10-28 23:39:46 +0700

    Move global styles

---
+++ styles/global-styles.js
@@ -0,0 +1,42 @@
+// External modules
+import { css } from '@emotion/react';
+
+const globalStyles = css`
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+  }
+
+  body {
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, sans-serif;
+    line-height: 1;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    margin: 0;
+    font-size: 100%;
+    font: inherit;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  ol,
+  ul {
+    list-style: none;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`;
+
+export default globalStyles;
---
+++ styles/index.js
@@ -0,0 +1,1 @@
+export { default as globalStyles } from '@/styles/global-styles';
