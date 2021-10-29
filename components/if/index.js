b72ad20366f2075f0be80bac94d421f8585e6249
Muhamad Fajri <hurifajri@gmail.com>
2021-10-29 18:43:55 +0700

    Update If component

--- components/if/index.js
+++ components/if/index.js
@@ -4,13 +4,14 @@ import PropTypes from 'prop-types';
 const If = ({ children, condition }) => {
   if (!condition) return null;
 
-  // Render children if condition is true
+  // Render children if condition is truthy
   return children;
 };
 
 If.propTypes = {
   children: PropTypes.oneOfType([PropTypes.node, PropTypes.element]).isRequired,
-  condition: PropTypes.bool.isRequired,
+  condition: PropTypes.oneOfType([PropTypes.bool, PropTypes.oneOf([undefined])])
+    .isRequired,
 };
 
 export default If;
