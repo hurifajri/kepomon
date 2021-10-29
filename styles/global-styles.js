f2ffda21bc6bc5fce47f241fc48dfc8028535c51
Muhamad Fajri <hurifajri@gmail.com>
2021-10-29 22:55:10 +0700

    Add css variables

--- styles/global-styles.js
+++ styles/global-styles.js
@@ -43,6 +43,19 @@ const globalStyles = css`
   * {
     box-sizing: border-box;
   }
+
+  :root {
+    --color-dark-black: #161a2b;
+    --color-dark-cyan: #a0cece;
+    --color-dark-green: #a6d5ab;
+    --color-dark-purple: #b0afdb;
+    --color-dark-white: #ffffff;
+    --color-light-blue: #5c7aff;
+    --color-light-cyan: #c6eced;
+    --color-light-green: #c7edca;
+    --color-light-purple: #c9c7ee;
+    --color-light-white: hsl(0, 0%, 100%, 0.5);
+  }
 `;
 
 export default globalStyles;
