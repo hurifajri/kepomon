b81fe251e19546f08aa4e1d2aac2d073cfceab80
Muhamad Fajri <hurifajri@gmail.com>
2021-10-30 22:59:54 +0700

    Update Navbar component

--- components/navbar/style.js
+++ components/navbar/style.js
@@ -1,5 +1,6 @@
 // External modules
 import { css } from '@emotion/css';
+import { keyframes } from '@emotion/react';
 import styled from '@emotion/styled';
 
 export const StyledNav = styled.nav``;
@@ -21,8 +22,24 @@ export const StyledRoute = styled.span`
     3px 3px 0 var(--dark-black);
 `;
 
+const rotate = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(-720deg);
+  }
+`;
+
 export const cardContentStyle = css`
   display: flex;
   align-items: center;
   margin: 6px -10px 5px 35px;
+
+  &:hover svg {
+    animation: ${rotate} ease-in-out 0.5s;
+    animation-delay: 0.05s;
+    animation-iteration-count: inherit;
+    transform-origin: 50% 50%;
+  }
 `;
