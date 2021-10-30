96db5269dab332017f97e3854bd411c9781b7f65
Muhamad Fajri <hurifajri@gmail.com>
2021-10-30 19:04:47 +0700

    Update App component

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,16 @@ import { ApolloProvider } from '@apollo/client';
 import { Global } from '@emotion/react';
 
 // Internal modules
+import Layout from '@/components/layout';
 import client from '@/graphql/client';
 import globalStyles from '@/styles/global-styles';
 
 const App = ({ Component, pageProps }) => (
   <ApolloProvider client={client}>
     <Global styles={globalStyles} />
-    <Component {...pageProps} />
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   </ApolloProvider>
 );
 
