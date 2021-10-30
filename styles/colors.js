bb68e7f56c177fdd751778c01feb469e701d887a
Muhamad Fajri <hurifajri@gmail.com>
2021-10-30 19:03:49 +0700

    Create colors helper

---
+++ styles/colors.js
@@ -0,0 +1,15 @@
+export const lightColors = {
+  blue: 'var(--light-blue)',
+  cyan: 'var(--light-cyan)',
+  green: 'var(--light-green)',
+  purple: 'var(--light-purple)',
+  white: 'var(--light-white)',
+};
+
+export const darkColors = {
+  black: 'var(--dark-black)',
+  cyan: 'var(--dark-cyan)',
+  green: 'var(--dark-green)',
+  purple: 'var(--dark-purple)',
+  white: 'var(--dark-white)',
+};
