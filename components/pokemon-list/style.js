83fc3fdcff60b5f10a7e9db031c29f13b5e8b08e
Muhamad Fajri <hurifajri@gmail.com>
2021-10-30 13:58:15 +0700

    Update Pokemon List component

--- components/pokemon-list/style.js
+++ components/pokemon-list/style.js
@@ -1,10 +1,16 @@
 // External modules
 import styled from '@emotion/styled';
 
+// Internal modules
+import media from '@/styles/media';
+
 export const StyledList = styled.ul`
   display: flex;
   align-items: center;
   justify-content: center;
+  ${media.xs} {
+    justify-content: space-between;
+  }
   gap: 35px;
   flex-wrap: wrap;
 `;
