758b0215041a42295f4f34bdbc0ff4a4bc7fb819
Muhamad Fajri <hurifajri@gmail.com>
2021-10-30 11:24:55 +0700

    Add ApolloProvider to app page

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,16 @@
 // External modules
+import { ApolloProvider } from '@apollo/client';
 import { Global } from '@emotion/react';
 
 // Internal modules
+import client from '@/graphql/client';
 import globalStyles from '@/styles/global-styles';
 
 const App = ({ Component, pageProps }) => (
-  <>
+  <ApolloProvider client={client}>
     <Global styles={globalStyles} />
     <Component {...pageProps} />
-  </>
+  </ApolloProvider>
 );
 
 export default App;
