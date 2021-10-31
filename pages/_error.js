543594b844119606f551a22c319b5d1b589569c2
Muhamad Fajri <hurifajri@gmail.com>
2021-10-31 11:13:58 +0700

    Update Error component

--- pages/_error.js
+++ pages/_error.js
@@ -1,12 +1,10 @@
-const Error = ({ statusCode }) => {
-  return (
-    <p>
-      {statusCode
-        ? `An error ${statusCode} occurred on server`
-        : 'An error occurred on client'}
-    </p>
-  );
-};
+const Error = ({ statusCode }) => (
+  <p>
+    {statusCode
+      ? `An error ${statusCode} occurred on server`
+      : 'An error occurred on client'}
+  </p>
+);
 
 Error.getInitialProps = ({ res, err }) => {
   const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
