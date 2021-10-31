71e88ad52089a78e95cf42aba8629b9bd5233548
Muhamad Fajri <hurifajri@gmail.com>
2021-10-31 22:38:31 +0700

    Create PokemonMoveList component

---
+++ components/pokemon-move-list/style.js
@@ -0,0 +1,11 @@
+// External modules
+import { css } from '@emotion/react';
+
+export const listStyle = css`
+  display: flex;
+  justify-content: flex-start;
+  gap: 5px;
+  max-height: 412px;
+  overflow-y: scroll;
+  padding: 5px;
+`;
