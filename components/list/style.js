6c390ec8ed5982a37a5e0c460c4b2b2219ae7088
Muhamad Fajri <hurifajri@gmail.com>
2021-10-31 22:16:20 +0700

    Update List component

--- components/list/style.js
+++ components/list/style.js
@@ -1,9 +1,6 @@
 // External modules
 import { css } from '@emotion/react';
 
-// Internal modules
-import media from '@/styles/media';
-
 export const listStyle = css`
   display: flex;
   align-items: center;
@@ -11,9 +8,6 @@ export const listStyle = css`
   padding: 0;
   margin: 0;
   list-style: none;
-  ${media.sm} {
-    justify-content: space-between;
-  }
   gap: 35px;
   flex-wrap: wrap;
 `;
