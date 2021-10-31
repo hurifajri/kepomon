7716b573a19860f330261e684ba4b0538e1e8567
Muhamad Fajri <hurifajri@gmail.com>
2021-10-31 14:46:31 +0700

    Update colors usage

--- styles/colors.js
+++ styles/colors.js
@@ -1,15 +1,16 @@
-export const lightColors = {
-  blue: 'var(--light-blue)',
-  cyan: 'var(--light-cyan)',
-  green: 'var(--light-green)',
-  purple: 'var(--light-purple)',
-  white: 'var(--light-white)',
-};
-
 export const darkColors = {
   black: 'var(--dark-black)',
   cyan: 'var(--dark-cyan)',
   green: 'var(--dark-green)',
   purple: 'var(--dark-purple)',
   white: 'var(--dark-white)',
+};
+
+export const lightColors = {
+  black: 'var(--light-black)',
+  blue: 'var(--light-blue)',
+  cyan: 'var(--light-cyan)',
+  green: 'var(--light-green)',
+  purple: 'var(--light-purple)',
+  white: 'var(--light-white)',
 };
--- styles/global-styles.js
+++ styles/global-styles.js
@@ -24,11 +24,12 @@ const globalStyles = css`
     --dark-green: #a6d5ab;
     --dark-purple: #b0afdb;
     --dark-white: #ffffff;
+    --light-black: hsla(0, 0%, 0%, 0.05);
     --light-blue: #5c7aff;
     --light-cyan: #c6eced;
     --light-green: #c7edca;
     --light-purple: #c9c7ee;
-    --light-white: hsl(0, 0%, 100%, 0.5);
+    --light-white: hsla(0, 0%, 100%, 0.5);
   }
 `;
 
