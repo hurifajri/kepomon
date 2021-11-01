577bbd06fdd8f52de82036d78f4b87362e47eb19
Muhamad Fajri <hurifajri@gmail.com>
2021-11-02 05:16:24 +0700

    Config pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
-module.exports = {
+// External modules
+const withPWA = require('next-pwa');
+
+const config = {
   images: {
     domains: ['raw.githubusercontent.com'],
   },
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+  },
+
   reactStrictMode: true,
 };
+
+module.exports = withPWA(config);
