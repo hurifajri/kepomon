3f83647034ebbf9b9a57a0a26bb81f99841ed9d0
Muhamad Fajri <hurifajri@gmail.com>
2021-11-02 06:16:10 +0700

    Add PWA runtimeCaching

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 // External modules
 const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
 
 const config = {
   images: {
@@ -8,6 +9,7 @@ const config = {
   pwa: {
     dest: 'public',
     disable: process.env.NODE_ENV === 'development',
+    runtimeCaching,
   },
 
   reactStrictMode: true,
