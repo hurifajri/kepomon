b81595a7367f42e0cec62a2bb42b4cdcc0abfcf1
Muhamad Fajri <hurifajri@gmail.com>
2021-11-02 08:01:46 +0700

    Unit test setup

---
+++ jest.setup.js
@@ -0,0 +1,6 @@
+// Optional: configure or set up a testing framework before each test.
+// If this file is deleted, need to remove `setupFilesAfterEnv` from `jest.config.js`
+
+// Internal modules
+// Used for __tests__/testing-library.js
+import '@testing-library/jest-dom/extend-expect';
