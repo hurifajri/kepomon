5d1c31d5bd917760e37fc91f2b9291cae099514c
Muhamad Fajri <hurifajri@gmail.com>
2021-11-18 15:56:08 +0700

    Change endpoint

--- graphql/constants.js
+++ graphql/constants.js
@@ -1,4 +1,4 @@
-export const URI = 'https://graphql-pokeapi.graphcdn.app';
+export const URI = 'https://graphql-pokeapi.vercel.app/api/graphql';
 export const PER_PAGE = 30;
 export const DEFAULT_OFFSET = 0;
 export const VARIABLES = { limit: PER_PAGE, offset: DEFAULT_OFFSET };
